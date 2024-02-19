349302705070b9b6fc74b973f8339e0eeb6f433b
Hakan Can <hakancn297@gmail.com>
2024-02-19 09:52:41 +0300

    gallery-project

---
+++ Gallery-React-master/README.md
@@ -0,0 +1,63 @@
+# PixGallery - React Galeri Projesi
+
+PixGallery, React ve Vite ile geliştirilmiş bir stok fotoğraf uygulamasıdır. Bu projede React Hooks (useState, useEffect), React Modal, React Router ve Tailwind CSS gibi teknolojiler kullanılmıştır.
+
+## Özellikler
+
+- Kullanıcılar istedikleri anahtar kelimeler ile arama yapabilir.
+- Kullanıcılar fotoğrafları cihazlarına kaydedebilir.
+- Kullanıcılar fotoğrafları sosyal medyada paylaşabilir.
+
+## Kullanılan Teknolojiler
+
+- React: Kullanıcı arayüzünün geliştirilmesi için kullanıldı.
+- Vite: Hızlı ve modern bir geliştirme ortamı sağlamak için kullanıldı.
+- React Icons: Temiz ve özelleştirilebilir ikonlar eklemek için kullanıldı.
+- React Router: Tek sayfa uygulaması (SPA) navigasyonu sağlamak için kullanıldı.
+- React Modal : Fotoğrafları hızlı bir şekilde önizlemek için kullanıldı.
+- React Share : Fotoğrafları paylaşmak için kullanıldı.
+- Tailwind CSS: Hızlı ve yeniden kullanılabilir stil oluşturmak için kullanıldı.
+
+## Canlı Demo 
+### [gallery-react-eight.vercel.app](https://gallery-react-eight.vercel.app)
+
+## Nasıl Kullanılır
+
+1. Projeyi klonlayın:
+
+   ```bash
+   git clone https://github.com/measses/Gallery-React.git
+
+2. Proje dizinine gidin:
+   
+   ```bash
+      cd Gallery-React
+   ```
+3. Bağımlılıkları yükleyin:
+   ```bash
+     npm install
+   ```
+4. Uygulamayı başlatın
+    ```bash
+       npm run dev
+      ```
+
+## Ekran Görüntüleri
+--ANASAYFA--
+![ekran 1](https://github.com/measses/Gallery-React/assets/67739721/3e0b1f1d-c6ec-470a-99a1-6e5e0466f76b)
+--MODAL--
+![ekran 2](https://github.com/measses/Gallery-React/assets/67739721/52b6d6af-f2ff-44c3-ad69-8f91b9de1a9d)
+--FOTOĞRAF ARAMA-
+![ekran 3](https://github.com/measses/Gallery-React/assets/67739721/12251399-1466-45cc-b289-40456f021b71)
+--HATA--
+![hata](https://github.com/measses/Gallery-React/assets/67739721/bb41c7e4-8d8a-4c7b-8c86-5ac7c5e3b962)
+
+
+
+
+## Katkıda Bulunma
+Hata raporları ve öneriler için GitHub Pull Request atabilirsiniz.
+## Lisans
+Bu proje MIT lisansı altında lisanslanmıştır. Daha fazla bilgi için LICENSE dosyasına başvurun.
+
+
---
+++ Gallery-React-master/src/components/ImageCard.jsx
@@ -0,0 +1,80 @@
+import ModalImage from "react-modal-image";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+  WhatsappShareButton,
+} from "react-share";
+import {
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  WhatsappIcon,
+} from "react-share";
+
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
+
+  // Paylaşılacak URL'yi belirleyin
+  const shareUrl = image.largeImageURL;
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <ModalImage
+        className="w-full h-64 object-cover"
+        small={image.webformatURL}
+        large={image.largeImageURL}
+        alt={image.user}
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-royal-blue-500 text-xl mb-2">
+          Photo  {image.user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4 flex flex-wrap">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+      <div className="px-6 py-4">
+  <div className="grid grid-cols-2 mx-5 md:grid-cols-4 gap-2">
+    <FacebookShareButton url={shareUrl} className="mx-1">
+      <FacebookIcon size={32} round />
+    </FacebookShareButton>
+    <TwitterShareButton url={shareUrl} className="mx-1">
+      <TwitterIcon size={32} round />
+    </TwitterShareButton>
+    <LinkedinShareButton url={shareUrl} className="mx-1">
+      <LinkedinIcon size={32} round />
+    </LinkedinShareButton>
+    <WhatsappShareButton url={shareUrl} className="mx-1">
+      <WhatsappIcon size={32} round />
+    </WhatsappShareButton>
+  </div>
+</div>
+
+
+    </div>
+  );
+};
+
+export default ImageCard;
